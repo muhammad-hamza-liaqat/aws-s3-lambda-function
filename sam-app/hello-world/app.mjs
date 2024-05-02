7f9305e6d67753686458a9d765b529ead0217231
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-05-02 13:26:19 +0500

    utf-8 error

--- sam-app/hello-world/app.mjs
+++ sam-app/hello-world/app.mjs
@@ -1,30 +1,45 @@
-import aws from "aws-sdk";
-const s3 = new aws.S3();
+const AWS = require('aws-sdk');
 
-export const handler = async (event) => {
-  const bucketName = process.env.bucket_name;
-  const { image, description } = JSON.parse(event.body);
-  const imageContent = Buffer.from(image, "base64");
-  const params = {
-    Bucket: bucketName,
-    Key: Date.now().toString() + ".jpeg",
-    Body: imageContent,
-    ContentType: "image/jpeg",
-    Metadata: {
-      description: description,
-    },
-  };
-  try {
-    await s3.putObject(params).promise();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Image uploaded successfully!" }),
-    };
-  } catch (error) {
-    console.error("error in uploading image to s3", error || error.message);
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error || error.message),
-    };
-  }
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+    console.log('Received event:', JSON.stringify(event));
+
+    try {
+        const bucketName = process.env.BUCKET_NAME;
+        console.log('Bucket name:', bucketName);
+
+        // Extract image and description from req.body
+        const { image, description } = event.body;
+        const imageContent = Buffer.from(image, 'base64');
+        console.log('Description:', description);
+
+        const params = {
+            Bucket: bucketName,
+            Key: Date.now().toString() + '.png', 
+            Body: imageContent,
+            ContentType: 'image/png', 
+            Metadata: {
+                description: description,
+            },
+        };
+
+        console.log('Uploading image to S3:', JSON.stringify(params));
+
+        await s3.putObject(params).promise();
+
+        console.log('Image uploaded successfully');
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Image uploaded successfully!' }),
+        };
+    } catch (error) {
+        console.error('Error:', error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error uploading image to S3' }),
+        };
+    }
 };

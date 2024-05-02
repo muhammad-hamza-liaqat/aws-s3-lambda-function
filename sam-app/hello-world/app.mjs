974345b2c4066d70d7726ef4d453ee7ea1dd1f68
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-05-02 13:06:46 +0500

    s3 code added

--- sam-app/hello-world/app.mjs
+++ sam-app/hello-world/app.mjs
@@ -1,24 +1,30 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+import aws from "aws-sdk";
+const s3 = new aws.S3();
 
-export const lambdaHandler = async (event, context) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      })
-    };
-
-    return response;
+export const handler = async (event) => {
+  const bucketName = process.env.bucket_name;
+  const { image, description } = JSON.parse(event.body);
+  const imageContent = Buffer.from(image, "base64");
+  const params = {
+    Bucket: bucketName,
+    Key: Date.now().toString() + ".jpeg",
+    Body: imageContent,
+    ContentType: "image/jpeg",
+    Metadata: {
+      description: description,
+    },
   };
-  
+  try {
+    await s3.putObject(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Image uploaded successfully!" }),
+    };
+  } catch (error) {
+    console.error("error in uploading image to s3", error || error.message);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error || error.message),
+    };
+  }
+};
